4660d8eeb26c7f39611b5b25c55423cadfa3b7b8
Md Rakib <rakibsoft800@gmail.com>
2020-09-28 17:56:35 +0600

    Add Report Module

---
+++ PersonalInformationApp/Pims.Core/ReportModel/AllEmplyeeReport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pims.Core.ReportModel
+{
+    public class AllEmplyeeReport
+    {
+        public string EmployeId { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNo { get; set; }
+
+        public string Email { get; set; }
+
+        public string Addrees { get; set; }
+
+        public string BloodGroup { get; set; }
+
+        public string NationalId { get; set; }
+
+    }
+}
---
+++ PersonalInformationApp/Pims.Service/Manager/ReportModule/AllEmployeReportDataManager.cs
@@ -0,0 +1,49 @@
+﻿using Pims.Core.ReportModel;
+using Pims.Persistense.DatabaseFile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pims.Service.ReportModule
+{
+    public class AllEmployeReportDataManager
+    {
+        private PimsDbContext _dbContext;
+
+        public AllEmployeReportDataManager()
+        {
+            _dbContext = new PimsDbContext();
+        }
+
+        public IEnumerable<AllEmplyeeReport> GetData()
+        {
+            //Get Employee
+            var employes = _dbContext.GeneralInformations.ToList();
+
+            //List Define
+            var employeList = new List<AllEmplyeeReport>();
+
+            foreach(var emp in employes)
+            {
+                var employee = new AllEmplyeeReport()
+                {
+                    Name = emp.NameEnglish,
+                    PhoneNo = emp.PhoneNo,
+                    Email = emp.Email,
+                    BloodGroup = emp.BloodGroup,
+                    Addrees = emp.PresentAddress,
+                    NationalId = emp.NationalId,
+                    EmployeId = emp.EmployeId
+
+                };
+                employeList.Add(employee);
+            }
+            return employeList;
+
+        }
+
+
+    }
+}
