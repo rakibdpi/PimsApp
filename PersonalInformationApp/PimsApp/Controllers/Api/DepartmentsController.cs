eb287e9f6e81e03f609c33f804f6f2e34272eac3
Mehedi khan <mehedi.mk93@gmail.com>
2022-11-15 22:08:46 +0600

    change

--- PersonalInformationApp/PimsApp/Controllers/Api/DepartmentsController.cs
+++ PersonalInformationApp/PimsApp/Controllers/Api/DepartmentsController.cs
@@ -11,7 +11,7 @@ namespace PimsApp.Controllers.Api
 {
     public class DepartmentsController : ApiController
     {
-        //Manager's Object create from service
+        //Manager's Object create from service One
 
         DepartmentManager _manager = new DepartmentManager();
 
